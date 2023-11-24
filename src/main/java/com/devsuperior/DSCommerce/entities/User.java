fe867d86a199b838f27df2f3521656c543f54e91
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-11-24 13:21:29 -0300

    feat: Order

--- src/main/java/com/devsuperior/DSCommerce/entities/User.java
+++ src/main/java/com/devsuperior/DSCommerce/entities/User.java
@@ -3,6 +3,9 @@ package com.devsuperior.DSCommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "tb_user")
 public class User {
@@ -14,6 +17,9 @@ public class User {
     private String phone;
     private LocalDate birthDate;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    List<Order> orders = new ArrayList<>();
     //private String[] roles;
 
 
@@ -76,5 +82,9 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 }
