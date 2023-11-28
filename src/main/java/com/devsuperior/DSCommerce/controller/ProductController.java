6edbd422c9597042a9bf5c9b3dbf776ec9d6febb
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-11-28 09:51:23 -0300

    feat: add ProductController

---
+++ src/main/java/com/devsuperior/DSCommerce/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.devsuperior.DSCommerce.controller;
+
+import com.devsuperior.DSCommerce.DTO.ProductDTO;
+import com.devsuperior.DSCommerce.entities.Product;
+import com.devsuperior.DSCommerce.repositories.ProductRepository;
+import com.devsuperior.DSCommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+    @GetMapping(value = "{id}")
+    public ProductDTO findById(@PathVariable Long id) {
+        return productService.findById(id);
+    }
+}
