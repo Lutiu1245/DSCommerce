25ded93d01442281f88ad39dc6ffcdbe474168fb
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-11-29 09:53:35 -0300

    feat: busca paginada de produtos

--- src/main/java/com/devsuperior/DSCommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/DSCommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.devsuperior.DSCommerce.DTO.ProductDTO;
 import com.devsuperior.DSCommerce.entities.Product;
 import com.devsuperior.DSCommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +20,10 @@ public class ProductService {
     public ProductDTO findById(Long id) {
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
+    }
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> productList = repository.findAll(pageable);
+        return productList.map(x -> new ProductDTO(x));
     }
 }
