42d110729109ba0101bbd2273acd3d18306a1afd
Luan Henrique <99328979+Lutiu1245@users.noreply.github.com>
2023-11-30 09:28:04 -0300

    feat: created exeception

---
+++ src/main/java/com/devsuperior/DSCommerce/exceptions/ResourceNotFoundExeception.java
@@ -0,0 +1,8 @@
+package com.devsuperior.DSCommerce.exceptions;
+
+public class ResourceNotFoundExeception extends RuntimeException
+{
+    public ResourceNotFoundExeception(String msg) {
+        super(msg);
+    }
+}
--- src/main/java/com/devsuperior/DSCommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/DSCommerce/services/ProductService.java
@@ -2,6 +2,7 @@ package com.devsuperior.DSCommerce.services;
 
 import com.devsuperior.DSCommerce.DTO.ProductDTO;
 import com.devsuperior.DSCommerce.entities.Product;
+import com.devsuperior.DSCommerce.exceptions.ResourceNotFoundExeception;
 import com.devsuperior.DSCommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -18,7 +19,7 @@ public class ProductService {
     private ProductRepository repository;
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
-        Product product = repository.findById(id).get();
+        Product product = repository.findById(id).orElseThrow(() -> new ResourceNotFoundExeception("Recurso não encontrado"));
         return new ProductDTO(product);
     }
     @Transactional(readOnly = true)
